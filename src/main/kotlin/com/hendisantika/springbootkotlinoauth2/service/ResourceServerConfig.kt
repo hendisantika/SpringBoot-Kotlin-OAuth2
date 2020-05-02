25192c332e3d8044f6e48f716e80689bda7326a4
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-02 07:16:43 +0700

    #11 - Add corsConfigurationSource bean in ResourceServerConfig

--- src/main/kotlin/com/hendisantika/springbootkotlinoauth2/service/ResourceServerConfig.kt
+++ src/main/kotlin/com/hendisantika/springbootkotlinoauth2/service/ResourceServerConfig.kt
@@ -1,11 +1,15 @@
 package com.hendisantika.springbootkotlinoauth2.service
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +35,17 @@ class ResourceServerConfig : ResourceServerConfigurerAdapter() {
                 .and().cors()
                 .configurationSource(corsConfigurationSource())
                 .and().exceptionHandling().accessDeniedHandler(OAuth2AccessDeniedHandler())
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("*")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        config.allowCredentials = true
+        config.allowedHeaders = listOf("Content-Type", "Authorization")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        return source
     }
 }
