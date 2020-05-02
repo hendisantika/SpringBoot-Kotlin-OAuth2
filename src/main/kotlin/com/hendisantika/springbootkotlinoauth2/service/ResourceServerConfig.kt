aeff3c7e881b5cdc6167e3c215ab0ffdb3d7617b
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-02 07:16:08 +0700

    #10 - Add configure bean in ResourceServerConfig

--- src/main/kotlin/com/hendisantika/springbootkotlinoauth2/service/ResourceServerConfig.kt
+++ src/main/kotlin/com/hendisantika/springbootkotlinoauth2/service/ResourceServerConfig.kt
@@ -1,9 +1,11 @@
 package com.hendisantika.springbootkotlinoauth2.service
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,5 +21,15 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 class ResourceServerConfig : ResourceServerConfigurerAdapter() {
     override fun configure(resources: ResourceServerSecurityConfigurer) {
         resources.resourceId(RESOURCE_ID).stateless(false)
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.anonymous().disable()
+                .authorizeRequests()
+                .antMatchers("/ws/**").access("hasRole('ADMIN')")
+                .and().cors()
+                .configurationSource(corsConfigurationSource())
+                .and().exceptionHandling().accessDeniedHandler(OAuth2AccessDeniedHandler())
     }
 }
