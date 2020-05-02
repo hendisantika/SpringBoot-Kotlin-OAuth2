3db44670be26870fb080eae38af010459ccbcf09
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-02 07:22:23 +0700

    #15 - Add configure Bean in AuthorizationServerConfig

--- src/main/kotlin/com/hendisantika/springbootkotlinoauth2/security/AuthorizationServerConfig.kt
+++ src/main/kotlin/com/hendisantika/springbootkotlinoauth2/security/AuthorizationServerConfig.kt
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
 import org.springframework.security.oauth2.provider.token.TokenStore
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
@@ -40,5 +42,23 @@ class AuthorizationServerConfig : AuthorizationServerConfigurerAdapter() {
     @Bean
     fun tokenStore(): TokenStore? {
         return JwtTokenStore(accessTokenConverter())
+    }
+
+    @Throws(Exception::class)
+    override fun configure(configurer: ClientDetailsServiceConfigurer) {
+        configurer
+                .inMemory()
+                .withClient("henry-client")
+                .secret(passwordEncoder?.encode("henry-secret"))
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                .scopes("read", "write", "trust")
+                .accessTokenValiditySeconds(3600)
+                .refreshTokenValiditySeconds(3600)
+    }
+
+    override fun configure(endpoints: AuthorizationServerEndpointsConfigurer) {
+        endpoints.tokenStore(tokenStore())
+                .authenticationManager(authenticationManager)
+                .accessTokenConverter(accessTokenConverter())
     }
 }
