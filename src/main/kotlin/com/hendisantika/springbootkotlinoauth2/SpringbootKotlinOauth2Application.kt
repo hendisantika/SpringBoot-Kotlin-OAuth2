1c8040fe706029b6917322be36001476d3bedb1a
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-02 07:05:49 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/springbootkotlinoauth2/SpringbootKotlinOauth2Application.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.springbootkotlinoauth2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinOauth2Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootKotlinOauth2Application>(*args)
+}
---
+++ src/test/kotlin/com/hendisantika/springbootkotlinoauth2/SpringbootKotlinOauth2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootkotlinoauth2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinOauth2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
