cb5d57cfc12c2eadae1b5485d5d2ceca1d541276
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-02 07:12:19 +0700

    #7 - Add globalUserDetails & Configure Bean in SecurityConfig.kt

--- src/main/kotlin/com/hendisantika/springbootkotlinoauth2/security/SecurityConfig.kt
+++ src/main/kotlin/com/hendisantika/springbootkotlinoauth2/security/SecurityConfig.kt
@@ -1,12 +1,16 @@
 package com.hendisantika.springbootkotlinoauth2.security
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import javax.annotation.Resource
 
 /**
@@ -29,5 +33,26 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
     override fun authenticationManagerBean(): AuthenticationManager {
         return super.authenticationManagerBean()
+    }
+
+    @Autowired
+    @Throws(Exception::class)
+    fun globalUserDetails(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(encoder())
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http
+                .csrf().disable()
+                .anonymous().disable()
+                .authorizeRequests()
+                .antMatchers("/public/**").permitAll()
+    }
+
+    @Bean
+    fun encoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 }
