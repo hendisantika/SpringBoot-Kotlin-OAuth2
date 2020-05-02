6e23a25efd021830f61ad5585331d69c70e6d739
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-02 07:21:56 +0700

    #14 - Add accessTokenConverter & tokenStore Bean in AuthorizationServerConfig

--- src/main/kotlin/com/hendisantika/springbootkotlinoauth2/security/AuthorizationServerConfig.kt
+++ src/main/kotlin/com/hendisantika/springbootkotlinoauth2/security/AuthorizationServerConfig.kt
@@ -1,11 +1,15 @@
 package com.hendisantika.springbootkotlinoauth2.security
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.provider.token.TokenStore
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +29,16 @@ class AuthorizationServerConfig : AuthorizationServerConfigurerAdapter() {
 
     @Autowired
     private val passwordEncoder: BCryptPasswordEncoder? = null
+
+    @Bean
+    fun accessTokenConverter(): JwtAccessTokenConverter? {
+        val converter = JwtAccessTokenConverter()
+        converter.setSigningKey("he255xi")
+        return converter
+    }
+
+    @Bean
+    fun tokenStore(): TokenStore? {
+        return JwtTokenStore(accessTokenConverter())
+    }
 }
