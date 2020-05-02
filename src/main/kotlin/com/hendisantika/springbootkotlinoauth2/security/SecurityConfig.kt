3ed89ea21dd99e554482fad861dffaf905b19fc1
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-02 07:11:12 +0700

    #5 - Add SecurityConfig.kt

---
+++ src/main/kotlin/com/hendisantika/springbootkotlinoauth2/security/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootkotlinoauth2.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.UserDetailsService
+import javax.annotation.Resource
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-kotlin-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/05/20
+ * Time: 07.10
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    @Resource(name = "userService")
+    private val userDetailsService: UserDetailsService? = null
+
+}
